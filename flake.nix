e5c8493cf85938be0016a9911e097d588106d58a
hugosenari <hugosenari@gmail.com>
2022-03-12 01:07:54 -0300

    chore: re index chronos

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''An efficient library for asynchronous programming'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."chronos-master".url = "path:./master";
+  inputs."chronos-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."chronos-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
